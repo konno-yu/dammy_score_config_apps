3b638ad01552d2288b1d360927a17260377135d5
konno-yu <yuta.konno.socie@gmail.com>
2019-09-30 00:56:56 +0900

    テーブルで学生の情報を表示（インライン入力のサンプル作成まで）

--- src/StudentComponent.tsx
+++ src/StudentComponent.tsx
@@ -1,18 +1,29 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 import {
-    Form, 
+    Form,
     Input,
-    Button
+    Button,
+    Table,
+    Tag,
+    Divider,
+    Icon,
 } from "antd";
 import { FormComponentProps } from "antd/lib/form";
 import axios from "axios";
+import { TableComponents } from "antd/lib/table";
+import { render } from "react-dom";
 axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 
 
 type StudentType = {
     id: number,
-    name: string
+    studentNumber: string,
+    lastName: string,
+    firstName: string
+    faculityId: number,
+    classId: number,
+    entranceYear: number
 };
 type StudentFormProps<StudentType> = FormComponentProps;
 
@@ -20,8 +31,64 @@ type StudentFormProps<StudentType> = FormComponentProps;
 const LoginFormImpl: React.FC<StudentFormProps<StudentType>> = (props: StudentFormProps<StudentType>) => {
     const { form } = props;
     const { getFieldDecorator } = form;
+    const { Column, ColumnGroup } = Table;
+
     const [message, setMessage] = useState("");
     const [students, setStudents] = useState<StudentType[]>();
+    const [isEditMode, changeEditMode] = useState(false);
+    const [editingId, changeEditingId] = useState(-1);
+
+    const dammyData: StudentType[] = [
+        {
+            id: 1,
+            studentNumber: "GSB1000",
+            lastName: "ためかわ",
+            firstName: "ゆういち",
+            faculityId: 1,
+            classId: 3,
+            entranceYear: 2009
+        },
+        {
+            id: 2,
+            studentNumber: "GSB1001",
+            lastName: "おおかわ",
+            firstName: "ゆうじ",
+            faculityId: 1,
+            classId: 4,
+            entranceYear: 2009
+        }
+    ];
+
+    const columns = [
+        { title: "Id", dataIndex: "id", key: "id", editable: false},
+        { title: "StudentNumber", dataIndex: "studentNumber", key: "studentNumber", editable: true},
+        { title: "FirstName", dataIndex: "firstName", key: "firstName", editable: true},
+        { title: "ClassId", dataIndex: "classId", key: "classId", editable: true},
+        { title: "EntranceYear", dataIndex: "entranceYear", key: "entranceYear", editable: true},
+        { title: "", key: "action", render: (record: StudentType) => (
+            <span>
+            {
+                !isEditMode ?
+                    <span>
+                        <a onClick={() => {changeEditingId(record.id);changeEditMode(!isEditMode)}}><Icon type="edit"/></a>
+                        <Divider type="vertical" />
+                        <a><Icon type="delete"/></a>
+                    </span>
+                :
+                    <span>
+                        <a onClick={() => changeEditMode(!isEditMode)}><Icon type="save"/></a>
+                        <Divider type="vertical" />
+                        <a onClick={() => changeEditMode(!isEditMode)}><Icon type="alipay-circle"/></a>
+                    </span>
+            }
+            </span>
+        )}
+    ]
+
+    const editableCell = () => {
+
+    }
+
 
     useEffect(() => {
        loadStudents();
@@ -55,22 +122,58 @@ const LoginFormImpl: React.FC<StudentFormProps<StudentType>> = (props: StudentFo
         setMessage(event.target.value);
     }
 
-
+    const createCell = (text: string, record: StudentType, index: number) => (
+        <span>
+            {
+                (isEditMode && record.id === editingId)?
+                    <span>
+                    {
+                        getFieldDecorator("entranceIndex", {
+                            rules: [{required: true, message: "unko"}],
+                            initialValue: text
+                        })(<Input size="large" placeholder="入学年度"/>)
+                    }
+                    </span>
+                :
+                    <span>{record.classId}</span>
+            }
+        </span>
+    )
     return(
-        <div>
-        <Form onSubmit={handleSubmit}>
-            <Form.Item>
-                {getFieldDecorator("userName", {
-                    rules: [{required: true, message: "ユーザ名を入力してね"}]
-                })(<Input onChange={handleChange} placeholder="ユーザ名"/>)}
-            </Form.Item>
-            <Form.Item>
-                <Button type="primary" htmlType="submit">Log in</Button>
-            </Form.Item>
-        </Form>
-        { 
-            students && students.map((s) => <p>{s.name}</p>)
-        }
+        <div style={{width: '100%', margin: 10}}>
+        <Table dataSource={dammyData} columns={columns} bordered>
+            {/* <Column
+                title="入学年度"
+                dataIndex="entranceYear"
+                key="entranceYear"
+                render={(text, record: StudentType, index) => createCell(text, record, index)}/>
+            <Column title="学籍番号" dataIndex="studentNumber" key="studentNumber"/>
+            <Column title="姓" dataIndex="lastName" key="lastName"/>
+            <Column title="名" dataIndex="firstName" key="firstName"/>
+            <Column title="学部" dataIndex="faculityId" key="faculityId"/>
+            <Column title="クラス" dataIndex="classId" key="classId"/>
+            <Column
+                title=""
+                key="action"
+                render={(text, record: StudentType) => (
+                    <span>
+                    {
+                        !isEditMode ?
+                            <span>
+                                <a onClick={() => {changeEditingId(record.id);changeEditMode(!isEditMode)}}><Icon type="edit"/></a>
+                                <Divider type="vertical" />
+                                <a><Icon type="delete"/></a>
+                            </span>
+                        :
+                            <span>
+                                <a onClick={() => changeEditMode(!isEditMode)}><Icon type="save"/></a>
+                                <Divider type="vertical" />
+                                <a onClick={() => changeEditMode(!isEditMode)}><Icon type="alipay-circle"/></a>
+                            </span>
+                    }
+                    </span>
+                )}/> */}
+        </Table>
         </div>
     );
 }
